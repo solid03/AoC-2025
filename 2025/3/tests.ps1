0b4516287408a405858acc51bd0c67997195ffb4
Sean Bryla <seanbryla@gmail.com>
2025-12-08 23:58:20 -0500

    day 3

--- 2025/3/tests.ps1
+++ 2025/3/tests.ps1
@@ -1,11 +1,9 @@
-Describe Part1 {
-    It 'Finds highest joltage' {
+Describe Solution {
+    It 'Part 1' {
         & "$PSScriptRoot\part1.ps1" .\example.txt | Should -Be 357
     }
-}
 
-Describe Part2 {
-    It 'Finds highest joltage for 12' {
+    It 'Part 2' {
         & "$PSScriptRoot\part2.ps1" .\example.txt | Should -Be 3121910778619
     }
 }
